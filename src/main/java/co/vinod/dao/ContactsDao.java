ac93271697760c588a3a5baab0552685c6aeff34
BimanSupakar <61707034+BimanSupakar@users.noreply.github.com>
2020-04-24 16:03:39 +0530

    first commit for jaxrs-training

---
+++ src/main/java/co/vinod/dao/ContactsDao.java
@@ -0,0 +1,26 @@
+package co.vinod.dao;
+
+
+import java.util.List;
+
+import co.vinod.entity.Contact;
+
+public interface ContactsDao {
+
+	// CRUD OPERATION
+	public Contact addContact(Contact contact) throws DaoException;
+
+	public Contact findById(Integer id) throws DaoException;
+
+	public Contact updateContact(Contact contact) throws DaoException;
+
+	public void deleteContact(Integer id) throws DaoException;
+
+	// QUERIES
+
+	public List<Contact> findAll() throws DaoException;
+
+	public List<Contact> findByCity(String city) throws DaoException;
+
+	public List<Contact> findByCountry(String country) throws DaoException;
+}
